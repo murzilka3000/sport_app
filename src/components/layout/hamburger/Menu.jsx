c172dd4e735c19db4cf619cdd468827d5ad3bdec
Nikolay <nikolayzhidkov@icloud.com>
2024-05-29 03:35:57 +0300

    header

--- src/components/layout/hamburger/Menu.jsx
+++ src/components/layout/hamburger/Menu.jsx
@@ -1,29 +1,32 @@
 import React from 'react'
 import menu from './menu.data'
-import cn from 'clsx'
 import s from './Hamburger.module.scss'
+import cn from 'clsx'
+
 
 const Menu = ({isShow}) => {
 
     const handleLogout = () => {};
 
     return (
-        <nav className={cn(s.menu, {[s.show]: isShow})}>
-            <ul>
-                {
-                    menu.map((t, index) => {
-                        return(
-                            <li key={index}>
-                                {t.title}
-                            </li>
-                        )
-                    })
-                }
-                <li>
-                    <button onClick={handleLogout}>Logout</button>
-                </li>
-            </ul>
-        </nav>
+        <div className={s.wrapper}>
+            <nav className={cn(s.menu, {[s.show]: isShow})}>
+                <ul>
+                    {
+                        menu.map((t, index) => {
+                            return(
+                                <li key={index}>
+                                    {t.title}
+                                </li>
+                            )
+                        })
+                    }
+                    <li>
+                        <button onClick={handleLogout}>Logout</button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
