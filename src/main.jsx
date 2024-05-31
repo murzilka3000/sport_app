205dceed561474a376522ddca0abd747df082fbe
Nikolay <nikolayzhidkov@icloud.com>
2024-05-31 19:44:18 +0300

    bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/screens/home/Home'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/Routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+    Route,
+    Routes,
+    BrowserRouter,
+  } from "react-router-dom";
+import NotFound from '../components/screens/not-found/NotFound';
+import { routes } from './routes.data';
+  
+  const Routes = () => {
+    return (
+      <BrowserRouter>
+        <Routes>
+          {
+              routes.map(t => {
+                  return(
+                      <Route 
+                      key={t.path} 
+                      path={t.path} 
+                      element={<t.component />} 
+                      />
+                  )
+              })
+          }
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    )
+  }
+  
+  export default Routes
