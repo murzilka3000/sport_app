e6e90698a062b882240dd54eb4ea77107df8953f
Nikolay <nikolayzhidkov@icloud.com>
2024-06-02 18:31:35 +0300

    stopping until the backend is written

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,39 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../layout/Layout'
 import { useForm } from 'react-hook-form'
 import Button from '../../ui/button/Button'
 import Field from '../../ui/field/Field'
+import Loader from '../../ui/Loader'
+import s from './Auth.module.scss'
 
 const Auth = () => {
+
+  const isLoading = false;
+  const isLoadingAuth = false;
+
+  const [type, setType] = useState('auth')
  
-  const {register, handleSubmit, formState: {errors}} = useForm({
+  const { register, handleSubmit, formState: { errors } } = useForm({
     mode: 'onChange'
   })
 
-  const onSubmit = (data) => {
+  function onSubmit(data) {
     console.log(data)
   }
 
   return (
-    <Layout heading='Sign in' bgImage='/images/auth-bg.png'>
-      <div className="wrapper-inner-page">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Field
-          error={errors?.email?.message}
-          name='email'
-          register={register}
-          options={
-            {...register('email', {
-              required: 'Email is Required'
-            })}
-          }
-          type="text" 
-          placeholder='Enter email' 
-          />
-          <Button>Let's Go</Button>
-        </form>
-      </div>
-    </Layout>
+    <>
+      <Layout heading='Sign in' bgImage='/images/auth-bg.png'/>
+        <div className="wrapper-inner-page">
+          {(isLoading || isLoadingAuth) && <Loader/>}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Field
+              error={errors?.email?.message}
+              name='email'
+              register={register}
+              options={{
+                required: 'Email is Required'
+              }}
+              type="text" 
+              placeholder='Enter email' 
+            />
+            <Field
+              error={errors?.password?.message}
+              name='password'
+              register={register}
+              options={{
+                required: 'Password is Required'
+              }}
+              type="text" 
+              placeholder='Enter password' 
+            />
+            <div className={s.wrapperButtons}>
+              <Button clickHandler={()=> {setType('auth')}}>Sign in</Button>
+              <Button clickHandler={()=> {setType('reg')}}>Sign up</Button>
+            </div>
+          </form>
+        </div>
+      </>
   )
 }
 
---
+++ src/components/ui/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <img src="/images/three-dots.svg" alt="loader" draggable={false} width={90} />
+  )
+}
+
+export default Loader
